a4f963f327bb245f4401708e887ad297ebdeb320
Anas Khafagy <anas.mdhat@outlook.com>
2023-08-15 17:41:10 +0300

    enchancements

--- src/components/NavIcon.tsx
+++ src/components/NavIcon.tsx
@@ -1,22 +1,11 @@
 import React from 'react'
 
 export default function NavIcon({ normalIcon, hoverIcon }: any) {
-  const [isHover, setIsHover] = React.useState(false)
-
-  const onMouseEnter = () => setIsHover(true)
-  const onMouseLeave = () => setIsHover(false)
-
   return (
-    <div
-      onMouseEnter={onMouseEnter}
-      onMouseLeave={onMouseLeave}
-      className="w-12 justify-center flex "
-    >
+    <div className="w-12 justify-center flex group">
       <div className="absolute top-0 bottom-0 flex items-center">{normalIcon}</div>
       <div
-        className={`absolute top 0 bottom-0  ${
-          isHover ? 'opacity-100' : 'opacity-0'
-        } transition-opacity duration-500 ease-in-out`}
+        className={`absolute top 0 bottom-0 group-hover:opacity-100 opacity-0 transition-opacity duration-500 ease-in-out`}
       >
         {hoverIcon}
       </div>
